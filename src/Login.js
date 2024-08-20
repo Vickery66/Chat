e0aae6cbc308d3d24109e24f042cc5d9c2557c1f
Vickery66 <203322069@qq.com>
2024-08-20 10:10:04 +0800

    修改登录组件，将类组件改为函数组件

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
 import {useQuery,gql,useApolloClient,useMutation} from '@apollo/client'; 
 import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 const SignUp=gql`
     mutation($username: String!, $email: String!, $password: String!){
   signUp(username: $username, email: $email, password: $password)
@@ -64,6 +66,69 @@ export class Login extends React.Component{
         )
     }
 }
+
+
+// export function Login(){
+//     const [signUp] = useMutation(SignUp,{onCompleted:data=>{
+//         localStorage.setItem('token',data.signUp);
+//      }});
+//     const [signIn] = useMutation(SignIn,{onCompleted:data=>{
+//         localStorage.setItem('token',data.signIn);
+//     }});
+//     const navigate=useNavigate();
+//     const [username,setUsername]=useState('');
+//     const [email,setEmail]=useState('');
+//     const [password,setPassword]=useState('');
+//     const handleUsernameChange=(e)=>{
+//         setUsername(e.target.value);
+//     }
+//     const handleEmailChange=(e)=>{
+//         setEmail(e.target.value);
+//     }
+//     const handlePasswordChange=(e)=>{
+//         setPassword(e.target.value);
+//     }
+//     const handleSignIn=()=>{
+//         localStorage.setItem("username",username);
+//         localStorage.setItem("email",email);
+//         localStorage.setItem("password",password);
+//         signIn({variables:{
+//             username:username,
+//             email:email,
+//             password:password}}
+//         );
+//         navigate('/Home/*');
+//     }
+//     const handleSignUp=()=>{
+//         localStorage.setItem("username",username);
+//         localStorage.setItem("email",email);
+//         localStorage.setItem("password",password);
+//         signUp({variables:{
+//             username:username,
+//             email:email,
+//             password:password}}
+//         );
+//         navigate('/Home/*');
+//     }
+//     return(
+//         <form className="form">
+//             <span>
+//                 <label> username:</label>
+//                 <input type='text' value={username} onChange={handleUsernameChange}/>
+//             </span>
+//             <span>
+//                 <label>    email:</label>
+//                 <input type='text' value={email} onChange={handleEmailChange}/>
+//             </span>
+//             <span>
+//                 <label> password:</label>
+//                 <input type='password' value={password} onChange={handlePasswordChange}/>
+//             </span>
+//             <button className='btn1' onClick={handleSignUp}>创建</button>
+//             <button className='btn2' onClick={handleSignIn}>登录</button>
+//         </form>
+//     );
+// }
 
 export function Login_UP(){
      const [signUp, { data, loading, error }] = useMutation(SignUp,{onCompleted:data=>{
