f430e9365d563aea67e82cc0709f01a9a913c2e3
Vickery66 <203322069@qq.com>
2024-08-20 10:51:58 +0800

    修改Header组件，改为函数组件

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 const name="E-Chat";
-export class Header1 extends React.Component{
-    constructor(props)
-    {
-        super(props);
-    }
-    render(){
-        return (
-            <header className='header1'>{name}</header>
-        )
-    }
+// export class Header1 extends React.Component{
+//     render(){
+//         return (
+//             <header className='header1'>{name}</header>
+//         )
+//     }
+// }
+
+export function Header1(){
+    return(
+        <header className='header1'>{name}</header>
+    );
 }
