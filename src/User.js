e85be3a705ae14c6c6fbf4ecab180baa46c0504e
Vickery66 <203322069@qq.com>
2024-08-20 11:04:37 +0800

    修改User组件,改为函数组件

--- src/User.js
+++ src/User.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'; 
+import { useState } from 'react';
+// export class User extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             username:props.data.username,
+//             email:props.data.email,
+//             id:props.data.id
+//         }
+//         this.handleClick=this.handleClick.bind(this);
+//     }
+//     handleClick(e){
+//         e.preventDefault();
+//         localStorage.setItem("aim_username",this.state.username);
+//         localStorage.setItem("aim_toid",this.state.id);
+//     }
+//     render(){
+//         return(   
+//             <div className='user' onClick={this.handleClick}>
+//                 <span className='Name'>{this.state.username}</span>
+//                 <span className='Email'>{this.state.email}</span>
+//             </div>
+//         )
+//     }
+// }
 
-export class User extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            username:props.data.username,
-            email:props.data.email,
-            id:props.data.id
-        }
-        this.handleClick=this.handleClick.bind(this);
+export const User=({data})=>{
+    const [username,setUsername]=useState(data.username);
+    const [email,setEmail]=useState(data.email);
+    const [id,setId]=useState(data.id);
+    const handleClick=()=>{
+        localStorage.setItem("aim_username",username);
+        localStorage.setItem("aim_toid",id);
     }
-    handleClick(e){
-        e.preventDefault();
-        localStorage.setItem("aim_username",this.state.username);
-        localStorage.setItem("aim_toid",this.state.id);
-    }
-    render(){
-        return(   
-            <div className='user' onClick={this.handleClick}>
-                <span className='Name'>{this.state.username}</span>
-                <span className='Email'>{this.state.email}</span>
-            </div>
-        )
-    }
+    useEffect(()=>{
+        setUsername(data.username);
+        setEmail(data.email);
+        setId(data.id);
+    },[data]);
+    return(   
+        <div className='user' onClick={handleClick}>
+            <span className='Name'>{username}</span>
+            <span className='Email'>{email}</span>
+        </div>
+    );
 }
-
