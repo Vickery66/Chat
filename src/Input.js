9ffb38b352c609fd9b891f52363f94ea8594e12c
Vickery66 <203322069@qq.com>
2024-08-20 10:38:59 +0800

    修改输入组件，改为函数组件

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useQuery,gql,useApolloClient,useMutation} from '@apollo/client'; 
-
+import { useState } from 'react';
 const New_Note=gql`
     mutation newNote($content: String!, $to: String!){
   newNote(content: $content, to: $to){
@@ -48,5 +48,36 @@ export class Input extends React.Component{
         )
     }
 }
+
+
+// export function Input(){
+//     const [newNote, { data, loading, error }] = useMutation(New_Note);
+//     const [value,setValue]=useState('');
+//     const handleChange=(e)=>{
+//         setValue(e.target.value);
+//     }
+//     const handleClick=()=>{
+//         newNote({variables:{
+//             content:value,
+//             to:localStorage.getItem("aim_toid")
+//         }});
+//         setValue('');
+//     }
+//     const handleKeyDown=(e)=>{
+//         if(e.key==='Enter'){
+//             newNote({variables:{
+//                 content:value,
+//                 to:localStorage.getItem("aim_toid")
+//             }});
+//             setValue('');
+//         }
+//     }
+//     return(
+//         <>
+//         <input type="text" className='input' value={value} onChange={handleChange} onKeyDown={handleKeyDown}/>
+//         <button className='send' onClick={handleClick}>发送</button>
+//         </>
+//     );
+// }
 
 
